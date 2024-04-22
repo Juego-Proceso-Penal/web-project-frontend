6455990e3880bbeda755590c6aa5aafa46eb0827
Ymanol Martinez Castillo <ymanolcastillo@gmail.com>
2024-04-21 23:39:52 -0400

    fisrt commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "ENJ-Juego Proceso Penal-0.1";
+const contentToCache = [
+    "Build/Build-Web-PROCESO.loader.js",
+    "Build/Build-Web-PROCESO.framework.js.unityweb",
+    "Build/Build-Web-PROCESO.data.unityweb",
+    "Build/Build-Web-PROCESO.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
